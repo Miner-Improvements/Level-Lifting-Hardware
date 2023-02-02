4150caf755e8c493656263d51d2966cd63bf03bb
ktournillon <ktournillon@att.net>
2023-02-02 14:46:37 -0600

    Now Including BNO08x Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
+#include <HardwareSerial.h>
+#include <Adafruit_BNO08x.h> 
+
+
+
+#define LED_RED 19
+#define LED_GREEN 18
+#define LED_BLUE 15
+#define UART_TX 17
+#define UART_RX 16
+
+
+HardwareSerial SerialPort(2); //use UART2
 
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(UART_TX, OUTPUT);
+  pinMode(UART_RX, INPUT);
+  SerialPort.begin(9600, SERIAL_8N1, UART_RX, UART_TX);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+if (SerialPort.available())
+  {
+    float position = SerialPort.read();
+  }
 }
