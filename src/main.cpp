14df4dda5b97f17af1755890ffb8b13188770d02
ktournillon <ktournillon@att.net>
2023-02-16 15:24:10 -0600

    Removed commented examples, keeping the #include

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,12 @@
-/*
-#include <Arduino.h>
-#include <BluetoothSerial.h>
-#include <HardwareSerial.h>
-#include <Adafruit_BNO08x.h> 
-*/
-
-
-/*
-#define LED_RED 19
-#define LED_GREEN 18
-#define LED_BLUE 15
-#define UART_TX 17
-#define UART_RX 16
-
-
-HardwareSerial SerialPort(2); //use UART2
-
-void setup() {
-
-  pinMode(LED_RED, OUTPUT);
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(UART_TX, OUTPUT);
-  pinMode(UART_RX, INPUT);
-  SerialPort.begin(9600, SERIAL_8N1, UART_RX, UART_TX);
-  
-}
-
-void loop() {
-if (SerialPort.available())
-  {
-    float position = SerialPort.read();
-  }
-}
-*/
-
-/*
-void setup()
-{
-pinMode(22, OUTPUT); // Set GPIO22 as digital output pin
-}
-
-void loop() 
-{
-digitalWrite(22, HIGH); // Set GPIO22 active high
-delay(1000);  // delay of one second
-digitalWrite(22, LOW); // Set GPIO22 active low
-delay(1000); // delay of one second
-}
-*/
-
 /* Test sketch for Adafruit BNO08x sensor in UART-RVC mode */
 
+#include "Adafruit_BNO08x.h"
 #include "Adafruit_BNO08x_RVC.h"
-#include <HardwareSerial.h>
+#include "HardwareSerial.h"
 
 Adafruit_BNO08x_RVC rvc = Adafruit_BNO08x_RVC();
 
 void setup() {
-  //HardwareSerial MySerial(1);
-  //MySerial.begin(115200, SERIAL_8N1, 3, 17);
   
   // Wait for serial monitor to open
   Serial.begin(115200);
@@ -113,96 +60,3 @@ void loop() {
 
   //  delay(200);
 }
-
-
-/*
-// Basic demo for readings from Adafruit BNO08x
-#include <Adafruit_BNO08x.h>
-
-// For SPI mode, we need a CS pin
-//#define BNO08X_CS 10
-//#define BNO08X_INT 9
-
-// For SPI mode, we also need a RESET 
-//#define BNO08X_RESET 5
-// but not for I2C or UART
-#define BNO08X_RESET -1
-
-Adafruit_BNO08x  bno08x(BNO08X_RESET);
-sh2_SensorValue_t sensorValue;
-
-void setup(void) {
-  Serial.begin(9600);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
-
-  Serial.println("Adafruit BNO08x test!");
-
-  // Try to initialize!
-  //if (!bno08x.begin_I2C()) {
-  if (!bno08x.begin_UART(&Serial1)) {  // Requires a device with > 300 byte UART buffer!
-  //if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
-    Serial.println("Failed to find BNO08x chip");
-    while (1) { delay(10); }
-  }
-  Serial.println("BNO08x Found!");
-
-  for (int n = 0; n < bno08x.prodIds.numEntries; n++) {
-    Serial.print("Part ");
-    Serial.print(bno08x.prodIds.entry[n].swPartNumber);
-    Serial.print(": Version :");
-    Serial.print(bno08x.prodIds.entry[n].swVersionMajor);
-    Serial.print(".");
-    Serial.print(bno08x.prodIds.entry[n].swVersionMinor);
-    Serial.print(".");
-    Serial.print(bno08x.prodIds.entry[n].swVersionPatch);
-    Serial.print(" Build ");
-    Serial.println(bno08x.prodIds.entry[n].swBuildNumber);
-  }
-
-  Serial.println("Setting desired reports");
-  if (! bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    Serial.println("Could not enable game vector");
-  }
-
-  Serial.println("Reading events");
-  delay(100);
-}
-
-// Here is where you define the sensor outputs you want to receive
-void setReports(void) {
-  Serial.println("Setting desired reports");
-  if (! bno08x.enableReport(SH2_GAME_ROTATION_VECTOR)) {
-    Serial.println("Could not enable game vector");
-  }
-}
-
-
-void loop() {
-  delay(10);
-
-  if (bno08x.wasReset()) {
-    Serial.print("sensor was reset ");
-    setReports();
-  }
-  
-  if (! bno08x.getSensorEvent(&sensorValue)) {
-    return;
-  }
-  
-  switch (sensorValue.sensorId) {
-    
-    case SH2_GAME_ROTATION_VECTOR:
-      Serial.print("Game Rotation Vector - r: ");
-      Serial.print(sensorValue.un.gameRotationVector.real);
-      Serial.print(" i: ");
-      Serial.print(sensorValue.un.gameRotationVector.i);
-      Serial.print(" j: ");
-      Serial.print(sensorValue.un.gameRotationVector.j);
-      Serial.print(" k: ");
-      Serial.println(sensorValue.un.gameRotationVector.k);
-      break;
-  }
-
-}
-
-*/
