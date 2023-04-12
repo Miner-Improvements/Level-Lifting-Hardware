4bc4cee16ef847dc0ba065e6e23a481ba02e1a78
ktournillon <ktournillon@att.net>
2023-04-11 20:14:25 -0500

    2x max value :(

--- include/BatteryManager.h
+++ include/BatteryManager.h
@@ -4,11 +4,12 @@
 #include <driver/adc.h>
 
 //read battery voltage per %
-extern int * battery_sum;                  // sum of samples taken
+extern int battery;
+extern int battery_sum;                 // sum of samples taken
 extern float battery_voltage;           // calculated voltage
 extern float battery_output;            // output value
-const float battery_max = 3.7; // maximum voltage of battery
-const float battery_min = 3.0; // minimum voltage of battery before shutdown
+const float battery_max = 0.3364; // maximum voltage of battery after voltage divider
+const float battery_min = 0.2727; // minimum voltage of battery before shutdown
 const float R1 = 100000.0;     // resistance of R1 (100K)
 const float R2 = 10000.0;      // resistance of R2 (10K)
 const float samples = 500.0;       // number of samples taken
